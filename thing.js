31441595d4a451ae087e110d7499df3d3d88af25
some-6ude <102697068+some-6ude@users.noreply.github.com>
2022-05-05 11:05:12 -0600

    Create thing.js

---
+++ thing.js
@@ -0,0 +1,12 @@
+        //variables
+        var test = prompt("what do you want to write");
+        var test3 = test + " ";
+        var test2 = "the text thats gonna get written is " + test;
+        //lazy basic js code
+        alert(test2);
+        alert(test);
+        alert("made by karim");
+        document.write(test);
+        document.write("\n");
+console.log("made by karim :)");
+document.title = test;
